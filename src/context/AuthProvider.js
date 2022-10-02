6f05cad32f423085b30f0569698acb593df81586
Daniel Markow <daniel.markow@outlook.com>
2022-10-02 17:44:16 +0200

    set up authentication context and custom hook to use it ergonomically, exiting users are stored in the users.js file

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,51 @@
+import {useState} from "react";
+
+// import {useNavigate, useLocation} from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
+
+import {users} from "../users";
+import {AuthContext} from "./AuthContext";
+
+
+function AuthProvider({children}) {
+  // const navigate = useNavigate();
+  // const location = useLocation();
+
+  const [token, setToken] = useState(null);
+
+  const authenticateUser = (credentials) => {
+    const userExists = users
+        .filter((u) =>
+            u.email === credentials.email && u.password === credentials.password
+        ).length !== 0
+    if (userExists) {
+      return uuidv4();
+    } else {
+      return null;
+    }
+  };
+
+  const handleLogin = (credentials) => {
+    const token = authenticateUser(credentials);
+    setToken(token);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+
+  const value = {
+    token,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+  };
+
+  return (
+      <AuthContext.Provider value={value}>
+        {children}
+      </AuthContext.Provider>
+  );
+
+}
+
+export default AuthProvider;
---
+++ src/users.js
@@ -0,0 +1,5 @@
+let users = [
+  {name: "daniel", email: "daniel@nonreliant.me", password: "test1234"},
+]
+
+export {users};
